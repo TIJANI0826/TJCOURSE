34a18f331d24b2c9353f8d1c23a607998accbfac
TIJANI0826 <ibrahimtijani08@gmail.com>
2021-08-03 01:41:42 +0100

    First commit to TJCOURSE - added courses and

---
+++ simplecourse/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
+from django.forms.utils import ValidationError
+#from lxml.html.clean import clean_html
+from django.utils.html import escape, mark_safe
+
+
+from .models import User,Student
+
+
+class TeacherSignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_teacher = True
+        if commit:
+            user.save()
+        return user
+
+
+class StudentSignUpForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_student = True
+        user.save()
+        student = Student.objects.create(user=user)
+        student.interests.add(*self.cleaned_data.get('interests'))
+        return user
+
+
+# class StudentInterestsForm(forms.ModelForm):
+#     class Meta:
+#         model = Student
+#         fields = ('interests', )
+#         widgets = {
+#             'interests': forms.CheckboxSelectMultiple
+#         }
+
+
